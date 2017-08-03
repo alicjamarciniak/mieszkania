0b7fa549ba291ee1ae67754eb2e8f2b4f14c7e67
alicjamarciniak <marciniak.al@gmail.com>
2017-08-03 22:34:59 +0200

    Webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    entry: `${__dirname}/src/app.js`,
+    output: {
+        path: `${__dirname}/dist`,
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            },
+        {
+            test: /\.scss$/,
+            use: ExtractTextPlugin.extract({
+            fallback: "style-loader",
+            use: ["css-loader", "sass-loader"],
+            publicPath: '/dist'
+            })
+        }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin("app.css")
+    ]
+}
